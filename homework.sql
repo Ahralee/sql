fea03074ae45cc99b0d336ae9c3318dc0226e7cf
Ahralee <lee.ahra@gmail.com>
2018-01-04 19:23:34 -0800

    Add files via upload

--- homework.sql
+++ homework.sql
@@ -1,6 +1,18 @@
+CREATE TABLE `user_score` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(32) DEFAULT NULL,
+  `last_name` varchar(32) DEFAULT NULL,
+  `score` int(11) DEFAULT '0',
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1
+
 SELECT * FROM test.user_score;
 
-
+INSERT INTO user_score (first_name, last_name, score) values ("Ahra", "Park", "100");
+INSERT INTO user_score (first_name, last_name, score) values ("Ahra", "Lee", "80");
+INSERT INTO user_score (first_name, last_name, score) values ("Hailey", "Yun", "100");
+INSERT INTO user_score (first_name, last_name, score) values ("Ryan", "Yun", "90");
 INSERT INTO user_score (first_name, last_name, score) values ("John", "Lee", "30");
 INSERT INTO user_score (first_name, last_name, score) values ("John", "Park", "20");
 INSERT INTO user_score (first_name, last_name, score) values ("John", "Yun", "10");
